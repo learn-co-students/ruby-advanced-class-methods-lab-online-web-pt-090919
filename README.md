c41d2fa65a9b79569d50eacd374c95755b9f5c12
Jalysa Reason <jalysareason@gmail.com>
2019-10-20 14:19:13 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ruby Advanced Class Methods Lab
+ok# Ruby Advanced Class Methods Lab
 
 ## Learning Goals
 
--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,27 @@
 class Song
-  attr_accessor :name, :artist_name
+ attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-
 end
+
+  def Song.create
+    song = Song.create
+    Song.all.include?("hot girl")
+  end
+  
+  
+  def Song.new_by_name("blinded")
+    song = song.new_by_name
+  end
+
+ 
+ 
+ 
+ 
